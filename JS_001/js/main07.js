6f46a8ffe2d220df4917102abed67ad883a7ca71
hanbyeol00 <gksquf6670@gmail.com>
2022-10-13 16:42:41 +0900

    배운것 + 숙제 main09

---
+++ JS_001/js/main07.js
@@ -0,0 +1,36 @@
+/**
+ * JS 에서 변수를 선언하는 방법
+ * const, let 키워드로 시작하는 방법
+ *
+ * const 변수명 = 값
+ *      한번 값을 보관하면 다른 값을 보관할수 없다
+ *
+ * let 변수명 = 값
+ *      값이 보관된 변수에 다른 값을 보관하면
+ *      원래 저장된 값은 사라진다
+ *      그리고 새롭게 보관한 값만 남아 있다
+ */
+const num1 = 30;
+const num2 = 40;
+// const : 값을 변경할수 없는 변수
+// 변수 : 데이터 보관장소
+// 할당 : 변수에 값을 보관
+// 변수 = 값
+document.write(num1 + ",");
+document.write(num2);
+document.write("<br />");
+
+let num3 = 100;
+let num4 = 200;
+// let : 값을 변경할수 있는 변수
+
+document.write(num3 + ",");
+document.write(num4);
+document.write("<br />");
+
+num3 = 50;
+num4 = 60;
+
+document.write(num3 + ",");
+document.write(num4);
+document.write("<br />");
---
+++ JS_001/js/main09.js
@@ -0,0 +1,13 @@
+const inputNum1 = document.querySelector("#num1");
+const inputNum2 = document.querySelector("#num2");
+const spanSum = document.querySelector("#sum");
+
+const btncacl = document.querySelector("button");
+
+btncacl?.addEventListener("click", () => {
+  const num1 = inputNum1.value;
+  const num2 = inputNum2.value;
+
+  spanSum.innerHTML = num1 + num2;
+  // innerText 를 이용해 값 구하기
+});
