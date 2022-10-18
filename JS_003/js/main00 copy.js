e24420c0b306946a513bdc1bac7ff97f9726b9dd
hanbyeol00 <gksquf6670@gmail.com>
2022-10-18 18:13:57 +0900

    22.10.18

---
+++ JS_003/js/main00 copy.js
@@ -0,0 +1,29 @@
+const but = document.querySelector("#bt");
+const relt1 = document.querySelector("#result1");
+const relt2 = document.querySelector("#result2");
+const relt3 = document.querySelector("#result3");
+const relt4 = document.querySelector("#result4");
+const inputNum1 = document.querySelector("#num1");
+const inputNum2 = document.querySelector("#num2");
+
+inputNum1.focus();
+
+but.addEventListener("click", () => {
+  const txtNum1 = inputNum1.value;
+  const txtNum2 = inputNum2.value;
+  const sum1 = Number(txtNum1);
+  const sum2 = Number(txtNum2);
+
+  if (!txtNum1) {
+    alert("정수1의 값을 입력하세요");
+    inputNum1.focus();
+  } else if (!txtNum2) {
+    alert("정수2의 값을 입력하세요");
+    inputNum2.focus();
+  } else {
+    relt1.innerText = txtNum1 + " + " + txtNum2 + " = " + (sum1 + sum2);
+    relt2.innerText = txtNum1 + " - " + txtNum2 + " = " + (sum1 - sum2);
+    relt3.innerText = txtNum1 + " x " + txtNum2 + " = " + sum1 * sum2;
+    relt4.innerText = txtNum1 + " ÷ " + txtNum2 + " = " + sum1 / sum2;
+  }
+});
